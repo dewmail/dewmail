7e0461db5fd80e6126d4d49a453a69a27078efe4
Stephen Parker <sparker@stephenparker.io>
2015-07-04 13:37:00 -0400

    Update byline.

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  * 
- *  Copyright (c) 2014 Stephen Parker (withaspark.com)
+ *  Copyright (c) 2014 Stephen Parker (stephenparker.io)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
